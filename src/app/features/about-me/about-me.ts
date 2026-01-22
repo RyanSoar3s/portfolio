cda35d93bfd8cb76deec90f5a7d684efe3ad1b9e
RyanSoar3s <ryansoares.dev@gmail.com>
2026-01-22 09:53:33 -0300

    feat create about-me component

---
+++ src/app/features/about-me/about-me.ts
@@ -0,0 +1,28 @@
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { Responsive } from '@services/responsive';
+
+@Component({
+  selector: 'app-about-me',
+  imports: [
+    CommonModule
+
+  ],
+  templateUrl: './about-me.html',
+  styleUrl: './about-me.scss'
+})
+export class AboutMe {
+  protected readonly responsive = inject(Responsive);
+  protected readonly techs = [
+    { tech: "HTML", styleClass: "html" },
+    { tech: "CSS", styleClass: "css" },
+    { tech: "Sass", styleClass: "sass" },
+    { tech: "Tailwind", styleClass: "tailwind" },
+    { tech: "Angular", styleClass: "angular" },
+    { tech: "NodeJS", styleClass: "nodejs" },
+    { tech: "Redis", styleClass: "redis" },
+    { tech: "MongoDB", styleClass: "mongodb" }
+
+  ];
+
+}
