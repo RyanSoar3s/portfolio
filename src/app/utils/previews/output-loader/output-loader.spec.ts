de7bf606f7a0e0342f7e8e14bb2b1879ba6d154b
RyanSoar3s <ryansoares.dev@gmail.com>
2026-01-29 10:39:14 -0300

    feat: create loader component to code box

---
+++ src/app/utils/previews/output-loader/output-loader.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { OutputLoader } from './output-loader';
+
+describe('OutputLoader', () => {
+  let component: OutputLoader;
+  let fixture: ComponentFixture<OutputLoader>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [OutputLoader]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(OutputLoader);
+    component = fixture.componentInstance;
+    await fixture.whenStable();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/utils/previews/output-loader/output-loader.ts
@@ -0,0 +1,31 @@
+import { CommonModule } from '@angular/common';
+import { Component, computed, inject, input, ViewEncapsulation } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+
+@Component({
+  selector: 'app-output-loader',
+  imports: [
+    CommonModule
+
+  ],
+  templateUrl: './output-loader.html',
+  styleUrl: './output-loader.scss',
+  host: {
+    '[style.backgroundColor]': `'white'`,
+    '[style.display]': `'block'`,
+    '[style.height]': `'260px'`,
+    '[style.width]': `'100%'`,
+    '[style.paddingTop]': `'5px'`,
+    '[style.overflow]': `'hidden'`
+
+  },
+  encapsulation: ViewEncapsulation.None
+})
+export class OutputLoader {
+  public html = input.required<string>();
+
+  private sanitizer = inject(DomSanitizer);
+
+  public sanitizedHtml = computed(() => this.sanitizer.bypassSecurityTrustHtml(this.html()));
+
+}
