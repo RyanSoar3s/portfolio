d06723777d8606edd08cd022dfcac439bede0621
RyanSoar3s <ryansoares.dev@gmail.com>
2026-02-06 17:41:15 -0300

    feat: add new project

--- src/app/features/projects/projects.ts
+++ src/app/features/projects/projects.ts
@@ -22,6 +22,16 @@ export class Projects {
   protected readonly projects = [
     {
       id: 0,
+      urlImage: "assets/images/guitar-academy-pro.png",
+      title: "Guitar Academy Pro",
+      description: "Landing page responsiva e moderna para venda de um curso completo de guitarra",
+      techs: [ "HTML 5", "CSS 3", "JavaScript", "Vite" ],
+      linkView: "https://landing-page-guitar-academy-pro.vercel.app/",
+      linkRepo: "https://github.com/RyanSoar3s/landing-page---guitar-academy-pro"
+
+    },
+    {
+      id: 1,
       urlImage: "assets/images/video-streaming.png",
       title: "Video Streaming",
       description: "Projeto de um serviço de streaming desenvolvido com foco em organização de conteúdos",
@@ -31,7 +41,7 @@ export class Projects {
 
     },
     {
-      id: 1,
+      id: 2,
       urlImage: "assets/images/soletre-game.png",
       title: "Soletre Game",
       description: "O Soletre Game é um jogo de palavras em português que desafia sua criatividade e conhecimento do idioma!",
@@ -39,17 +49,7 @@ export class Projects {
       linkView: "https://soletre-game.vercel.app/",
       linkRepo: "https://github.com/RyanSoar3s/soletre-game"
 
-    },
-    {
-      id: 2,
-      urlImage: "assets/images/loja-online.png",
-      title: "Loja Online",
-      description: "Este projeto é uma loja online simples desenvolvida em Angular",
-      techs: [ "HTML", "SASS", "ANGULAR" ],
-      linkView: "https://loja-online-peach.vercel.app/",
-      linkRepo: "https://github.com/RyanSoar3s/Loja-Online"
-
-    },
+    }
 
   ];
 
