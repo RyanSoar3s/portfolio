95147cc26f6eee5c79f93eead247655148c8d342
RyanSoar3s <ryansoares.dev@gmail.com>
2026-04-29 13:16:11 -0300

    feat: add internationalization support with Transloco and new language files

---
+++ transloco.config.ts
@@ -0,0 +1,10 @@
+import { TranslocoGlobalConfig } from '@jsverse/transloco-utils';
+
+const config: TranslocoGlobalConfig = {
+  rootTranslationsPath: 'public/i18n/',
+  langs: [ 'pt-BR', 'en' ],
+  keysManager: {}
+
+};
+
+export default config;
