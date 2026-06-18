073c87820adccd3bd75130510bf53970b31c2814
RyanSoar3s <ryansoares.dev@gmail.com>
2026-06-18 17:53:11 -0300

    refactor: remove unused output components and styles
    - Deleted OutputButton, OutputLoader, OutputReactiveInput, OutputTasks, and OutputUsers components along with their associated HTML, SCSS, and spec files.
    - Cleaned up related styles in utils/styles and removed unnecessary imports in styles.scss.

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -1,12 +1,10 @@
 import { TestBed } from '@angular/core/testing';
 import { App } from './app';
-import { translocoTestingProviders } from './testing/transloco-testing';
 
 describe('App', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [App],
-      providers: [translocoTestingProviders]
     }).compileComponents();
   });
 
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,45 +1,13 @@
-import { ViewportScroller } from '@angular/common';
-import { AfterViewInit, Component, ElementRef, inject, viewChildren } from '@angular/core';
-import { AboutMe } from '@features/about-me/about-me';
-import { Contacts } from '@features/contacts/contacts';
-import { Footer } from '@features/footer/footer';
-import { Header } from '@features/header/header';
-import { Home } from '@features/home/home';
-import { LanguageToggle } from '@features/language-toggle/language-toggle';
-import { Projects } from '@features/projects/projects';
-import { Technologies } from '@features/technologies/technologies';
-
+import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   imports: [
-    Header,
-    Home,
-    AboutMe,
-    Technologies,
-    Projects,
-    Contacts,
-    Footer,
-    LanguageToggle
+
 
   ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App implements AfterViewInit {
-  private sections = viewChildren<string, ElementRef<HTMLElement>>("section", { read: ElementRef<HTMLElement> });
-  private viewportScroll = inject(ViewportScroller);
-
-  ngAfterViewInit(): void {
-    const section = this.sections()[0].nativeElement;
-    this.viewportScroll.scrollToPosition([ 0, section.offsetTop - 120 ]);
-
-  }
-
-  protected scrollNavigation(index: number): void {
-    const section = this.sections()[index].nativeElement;
-
-    this.viewportScroll.scrollToPosition([ 0, section.offsetTop - 120 ]);
-
-  }
+export class App {
 
 }
