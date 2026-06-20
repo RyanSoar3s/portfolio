36939cd5b0b61e8741a08ccdbbcb2a911a99ba78
RyanSoar3s <ryansoares.dev@gmail.com>
2026-06-20 19:24:49 -0300

    refactor: enhance header component with responsive design and menu functionality

--- src/app/features/header/header.ts
+++ src/app/features/header/header.ts
@@ -1,6 +1,7 @@
-import { Component, computed, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faCode } from '@fortawesome/free-solid-svg-icons';
+import { faCode, faBars, faX } from '@fortawesome/free-solid-svg-icons';
+import { Responsive } from '@services/responsive';
 
 @Component({
   selector: 'app-header',
@@ -9,15 +10,27 @@ import { faCode } from '@fortawesome/free-solid-svg-icons';
 
   ],
   templateUrl: './header.html',
-  styleUrl: './header.scss'
+  styleUrl: './header.scss',
+  host: {
+    '[style.justify-content]': `(!(responsive.isXs() || responsive.isSm())) ? 'space-around' : 'space-between'`,
+    '[style.padding]': `(!(responsive.isXs() || responsive.isSm())) ? '' : '0px 30px'`,
+
+  }
 
 })
 export class Header {
+  responsive = inject(Responsive);
+
   protected readonly faCode = faCode;
+  protected readonly faBars = faBars;
+  protected readonly faX = faX;
+
   protected readonly photo = "assets/images/profile.png";
 
   protected readonly langs = [ "en", "pt" ];
 
   languageSelected = signal(this.langs[0]);
+
+  isOpenMenu = signal(false);
 
 }
