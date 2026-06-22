cbb9c021f43f3e86eb047768a15393bcc1ec5a12
RyanSoar3s <ryansoares.dev@gmail.com>
2026-06-22 20:28:11 -0300

    refactor: create code snippet component with HTML, SCSS, and TypeScript implementation

---
+++ src/app/features/shared/code-snippet/code-snippet.ts
@@ -0,0 +1,52 @@
+import { Component, signal, input, OnInit, computed } from '@angular/core';
+import { HighlightDirective } from '@directives/highlight';
+import type { CodeSnippetModel, FileName, Languages } from '@models/code.model';
+
+@Component({
+  selector: 'app-code-snippet',
+  imports: [
+    HighlightDirective
+
+  ],
+  templateUrl: './code-snippet.html',
+  styleUrl: './code-snippet.scss',
+  host: {
+    '[style.height]': 'height()',
+    '[style.width]': 'width()'
+
+  }
+})
+export class CodeSnippet implements OnInit{
+  protected height = input("440px");
+  protected width = input("532px");
+
+  codes = input.required<CodeSnippetModel>();
+
+  protected tabSelected = signal<string>("");
+  protected language = signal<Languages | undefined>(undefined);
+  
+  private codeSignal = signal("");
+  protected code = computed(() => this.codeSignal());
+
+
+  ngOnInit(): void {
+    const fileName = this.codes().tabs[0];
+    this.tabSelected.set(fileName);
+
+
+    const code = this.codes().codes[0][fileName]!;
+    this.language.set(code.type);
+    this.codeSignal.set(code.code);
+
+  }
+
+  changeCode(index: number, tab: FileName): void {
+    this.tabSelected.set(tab);
+
+    const code = this.codes().codes[index][tab]!;
+    this.language.set(code.type);
+    this.codeSignal.set(code.code);
+
+  }
+
+}
