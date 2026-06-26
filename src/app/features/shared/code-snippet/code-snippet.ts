ca230e507548103be9e6aa07186da79b432cb57b
RyanSoar3s <ryansoares.dev@gmail.com>
2026-06-26 19:24:07 -0300

    refactor: enhance code snippet component by improving rendering logic and updating styles

--- src/app/features/shared/code-snippet/code-snippet.ts
+++ src/app/features/shared/code-snippet/code-snippet.ts
@@ -1,4 +1,5 @@
-import { Component, signal, input, OnInit, computed, inject } from '@angular/core';
+import { Component, signal, input, OnInit, computed, inject, effect, type Type } from '@angular/core';
+import { NgComponentOutlet } from '@angular/common';
 import { HighlightDirective } from '@directives/highlight';
 import type { CodeSnippetModel, FileName, Languages } from '@models/code.model';
 import { Responsive } from '@services/responsive';
@@ -6,23 +7,25 @@ import { Responsive } from '@services/responsive';
 @Component({
   selector: 'app-code-snippet',
   imports: [
-    HighlightDirective
+    HighlightDirective,
+    NgComponentOutlet
 
   ],
   templateUrl: './code-snippet.html',
   styleUrl: './code-snippet.scss',
   host: {
     '[style.height]': 'height()',
-    '[style.width]': `(responsive.isXl() || responsive.isLg()) ? '45vw' : '90vw'`,
-    '[style.max-width]': 'width()'
+    '[style.width]': 'width()',
+    '[style.max-width]': 'maxWidth()'
 
   }
 })
 export class CodeSnippet implements OnInit {
   protected readonly responsive = inject(Responsive);
 
-  protected height = input("440px");
-  protected width = input("532px");
+  height = input.required<string>()
+  width = input.required<string>()
+  maxWidth = input<string>();
 
   codes = input.required<CodeSnippetModel>();
 
@@ -32,11 +35,23 @@ export class CodeSnippet implements OnInit {
   private codeSignal = signal("");
   protected code = computed(() => this.codeSignal());
 
+  protected render = signal<Type<unknown> | undefined>(undefined);
+
+  constructor() {
+    effect(() => {
+      const code = this.codes();
+
+      if (!code || !("render" in code) || typeof code.render !== "function") return;
+
+      code.render().then((component) => this.render.set(component));
+
+    });
+
+  }
 
   ngOnInit(): void {
     const fileName = this.codes().tabs[0];
     this.tabSelected.set(fileName);
-
 
     const code = this.codes().codes[0][fileName]!;
     this.language.set(code.type);
