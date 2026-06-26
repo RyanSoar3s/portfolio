a6814a4b4efedc14217a03595982d7a9074c2f0b
RyanSoar3s <ryansoares.dev@gmail.com>
2026-06-26 19:24:54 -0300

    refactor: integrate code snippet component and update tech panel styles

--- src/app/features/components/tech/tech.ts
+++ src/app/features/components/tech/tech.ts
@@ -4,11 +4,15 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faX } from '@fortawesome/free-solid-svg-icons';
 import { Language } from '@services/language';
 import contentByTranslation from '@utils/translation/translation';
+import { codeSnippetOutputHtml } from '@utils/code-snippets/snippets';
+import { CodeSnippetModel } from '@models/code.model';
+import { CodeSnippet } from '@shared/code-snippet/code-snippet';
 
 @Component({
   selector: 'app-tech',
   imports: [
-    FontAwesomeModule
+    FontAwesomeModule,
+    CodeSnippet
 
   ],
   templateUrl: './tech.html',
@@ -33,11 +37,23 @@ export class Tech {
 
   protected readonly faX = faX;
 
+  protected readonly outputComponents: Record<string, CodeSnippetModel> = {
+    "html5": codeSnippetOutputHtml
+
+  }
+
+  private codeSnippetSignal = signal<CodeSnippetModel | undefined>(undefined);
+  protected readonly codeSnippet = computed(() => this.codeSnippetSignal());
+
   selectTech(tech: string, pathUrl: string, indexDesc: number): void {
     if (!this.selectedTech() || this.selectedTech() !== tech) {
       this.selectedTech.set(tech);
       this.selectedTechLogoUrl.set(pathUrl);
       this.selectedDescIndex = indexDesc;
+
+      const key = (this.selectedTech()!).toLocaleLowerCase();
+      this.codeSnippetSignal.set(this.outputComponents[key]);
+
       return;
 
     }
