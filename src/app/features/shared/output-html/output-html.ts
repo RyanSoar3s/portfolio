5c030e6b9f7b9477513859c2099f312a2f7c8a25
RyanSoar3s <ryansoares.dev@gmail.com>
2026-06-26 19:23:29 -0300

    feat: add output HTML component with structured layout and content

---
+++ src/app/features/shared/output-html/output-html.ts
@@ -0,0 +1,11 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-output-html',
+  imports: [],
+  templateUrl: './output-html.html',
+  styles: '',
+  encapsulation: ViewEncapsulation.ShadowDom
+})
+export class OutputHtml {
+}
