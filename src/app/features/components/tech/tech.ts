9c9b8c963279408ab06efcb83a65465f6aafa65b
RyanSoar3s <ryansoares.dev@gmail.com>
2026-06-26 16:54:58 -0300

    refactor: simplify tech component by removing direct language handling and enhancing tech selection logic

--- src/app/features/components/tech/tech.ts
+++ src/app/features/components/tech/tech.ts
@@ -1,11 +1,14 @@
-import { Component, inject, input, signal } from '@angular/core';
-import { TranslocoModule, TranslocoService } from '@jsverse/transloco';
+import { Component, computed, inject, input, signal } from '@angular/core';
 import { Responsive } from '@services/responsive';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faX } from '@fortawesome/free-solid-svg-icons';
+import { Language } from '@services/language';
+import contentByTranslation from '@utils/translation/translation';
 
 @Component({
   selector: 'app-tech',
   imports: [
-    TranslocoModule
+    FontAwesomeModule
 
   ],
   templateUrl: './tech.html',
@@ -13,14 +16,40 @@ import { Responsive } from '@services/responsive';
 })
 export class Tech {
   protected readonly responsive = inject(Responsive);
-  protected readonly transloco = inject(TranslocoService);
+
+  private language = inject(Language);
+  protected tech = computed(() => {
+    const currLang = this.language.currLang() as keyof typeof contentByTranslation;
+
+    return contentByTranslation[currLang].tech;
+
+  });
 
   animate = input.required<boolean>();
 
   protected selectedTech = signal<string | undefined>(undefined);
+  protected selectedTechLogoUrl = signal<string | undefined>(undefined);
+  protected selectedDescIndex: number | undefined = undefined;
 
-  selectTech(tech: string): void {
-    this.selectedTech.set((!this.selectedTech() || this.selectedTech() !== tech) ? tech : undefined);
+  protected readonly faX = faX;
+
+  selectTech(tech: string, pathUrl: string, indexDesc: number): void {
+    if (!this.selectedTech() || this.selectedTech() !== tech) {
+      this.selectedTech.set(tech);
+      this.selectedTechLogoUrl.set(pathUrl);
+      this.selectedDescIndex = indexDesc;
+      return;
+
+    }
+
+    this.resetTech();
+
+  }
+
+  resetTech(): void {
+    this.selectedTech.set(undefined);
+    this.selectedTechLogoUrl.set(undefined);
+    this.selectedDescIndex = undefined;
 
   }
 
