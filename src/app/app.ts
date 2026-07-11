e4b41076267170e5eddee50dcc693dab6f936129
RyanSoar3s <ryansoares.dev@gmail.com>
2026-07-11 19:00:31 -0300

    refactor: replace scrollNavigation with activeComponents for improved navigation handling

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,5 @@
 import { ViewportScroller } from '@angular/common';
-import { Component, ElementRef, AfterViewInit, OnDestroy, viewChildren, signal, inject } from '@angular/core';
+import { Component, ElementRef, AfterViewInit, OnDestroy, viewChildren, inject, effect, signal } from '@angular/core';
 import { AboutMe } from '@components/about-me/about-me';
 import { Contacts } from '@components/contacts/contacts';
 import { Footer } from '@components/footer/footer';
@@ -7,6 +7,9 @@ import { Header } from '@components/header/header';
 import { Home } from '@components/home/home';
 import { Projects } from '@components/projects/projects';
 import { Tech } from '@components/tech/tech';
+import { ComponentsLabel } from '@models/components-label';
+import { StateComponents } from '@services/state-components';
+
 @Component({
   selector: 'app-root',
   imports: [
@@ -17,28 +20,41 @@ import { Tech } from '@components/tech/tech';
     Projects,
     Contacts,
     Footer
-
   ],
   templateUrl: './app.html',
   styleUrl: './app.scss',
   host: {
     '[style.overflow-x]': '"clip"'
-
   }
 })
 export class App implements AfterViewInit, OnDestroy {
   private viewportScroll = inject(ViewportScroller);
+  private stateComponents = inject(StateComponents);
 
   private childs = viewChildren("childs", { read: ElementRef<HTMLElement> });
+  private element = signal<HTMLElement | null>(null);
   private observer: IntersectionObserver | null = null;
-  protected readonly visibleComponents = signal({
-    "home": false,
-    "about-me": false,
-    "tech": false,
-    "projects": false,
-    "contacts": false
 
-  } as const);
+  constructor() {
+    effect(() => {
+      const element = this.element();
+
+      if (!element) return;
+
+      const label = element.getAttribute("aria-label");
+
+      if (!label) return;
+
+      const visibleComponents = this.stateComponents.visibleComponents();
+
+      if (!visibleComponents[label as ComponentsLabel].isActive) return;
+
+      this.viewportScroll.scrollToPosition([ 0, element.offsetTop - 100 ]);
+      this.element.set(null);
+
+    });
+
+  }
 
   ngAfterViewInit(): void {
     this.observer = new IntersectionObserver((entries) => {
@@ -50,33 +66,27 @@ export class App implements AfterViewInit, OnDestroy {
 
         if (!label) return;
 
-        this.updateVisibleComponents(label);
+        this.stateComponents.updateState(label as ComponentsLabel, "isTarget");
 
-      })
+      });
 
     },
     {
       threshold: 0.4
-
     });
 
     this.childs().forEach((child) => this.observer?.observe(child.nativeElement));
 
   }
 
-  protected scrollNavigation(index: number): void {
-    const childs = this.childs()[index].nativeElement;
+  protected activeComponents(index: number): void {
+    const keys = Object.keys(this.stateComponents.visibleComponents());
 
-    this.viewportScroll.scrollToPosition([ 0, childs.offsetTop - 100 ]);
+    keys
+      .slice(0, index + 1)
+      .forEach((k) => this.stateComponents.updateState(k as ComponentsLabel, "isTarget"));
 
-  }
-
-  private updateVisibleComponents(component: string): void {
-    this.visibleComponents.update((cmps) => ({
-      ...cmps,
-      [component]: true
-
-    }))
+    this.element.set(this.childs()[index].nativeElement as HTMLElement);
 
   }
 
